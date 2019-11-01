7ad16ac33907ba81076357547535cd8753802169
LushetskiyVlad <vlad.luzhetskiy@gmail.com>
2019-11-01 12:42:56 +0200

    menu toggle

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,16 +1,38 @@
 import React, {Component} from 'react';
+import Drawer from "../../components/Navigation/Drawer/Drawer";
+import MenuToggle from "../../components/Navigation/MenuToggle/MenuToggle";
 import classes from './Layout.module.css';
 
 class Layout extends Component {
-    render() {
-        return (
-            <div className={classes.Layout}>
-                <main>
-                    {this.props.children}
-                </main>
-            </div>
-        );
-    }
+  state = {
+    menu: false
+  };
+
+  toggleMenuHandler = () => {
+    this.setState({
+      menu: !this.state.menu
+    });
+  };
+  menuCloseHandler = () => {
+    this.setState({
+      menu: false
+    });
+  };
+
+  render() {
+    return (
+      <div className={classes.Layout}>
+        <Drawer isOpen={this.state.menu} onClose={this.menuCloseHandler}/>
+        <MenuToggle
+          onToggle={this.toggleMenuHandler}
+          isOpen={this.state.menu}
+        />
+        <main>
+          {this.props.children}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default Layout;
