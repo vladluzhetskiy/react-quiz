15d3cce4b0d0ece69896b7f78c98bd1d047f5b18
LushetskiyVlad <vlad.luzhetskiy@gmail.com>
2019-11-03 16:40:54 +0200

    sign up & sign in

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import is from 'is_js';
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
@@ -36,12 +37,34 @@ export default class Auth extends Component {
     }
   };
 
-  loginHandler = () => {
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    };
 
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDYISD4TWpgKyoilKvhEcE8ei_pNm3GGOA', authData);
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
-  registerHandler = () => {
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    };
 
+    try {
+      const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDYISD4TWpgKyoilKvhEcE8ei_pNm3GGOA', authData);
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   submitHandler = event => {
